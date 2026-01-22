068b9f8d361bc2eebc31961db2025d8981c9cad6
Dominik Purgał <amonsir29@gmail.com>
2026-01-22 21:12:46 +0100

    import old repo + port to 6000.3

---
+++ Assets/_vr_anatomy/Scripts/NetworkInteractableResetManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using Unity.Netcode.Components;
+
+namespace XRMultiplayer
+{
+    public class NetworkInteractableResetManager : NetworkBehaviour
+    {
+        [SerializeField]
+        private List<NetworkRigidbody> objects;
+
+        private Dictionary<NetworkRigidbody, (Vector3, Vector3, Quaternion)> transforms;
+        
+        private void Start()
+        {
+            objects ??=  new List<NetworkRigidbody>();
+            transforms ??= new Dictionary<NetworkRigidbody, (Vector3, Vector3, Quaternion)>();
+            
+            SaveTransforms();
+        }
+
+        private void SaveTransforms()
+        {
+            foreach (var  obj in objects)
+            {
+                transforms.Add(obj, (obj.transform.position, obj.transform.localScale, obj.transform.rotation));
+            }
+        }
+
+        public void ResetTransforms()
+        {
+            ResetTransformRpc();
+        }
+
+        [Rpc(SendTo.Server)]
+        protected void ResetTransformRpc()
+        {
+            foreach (var obj in objects)
+            {
+                var (pos, scale, rotation) = transforms[obj];
+                
+                obj.SetPosition(pos);
+                obj.SetRotation(rotation);
+                obj.transform.localScale = scale;
+            }
+        }
+    }
+}
